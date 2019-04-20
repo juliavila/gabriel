c8a20240f1c68d067301a702fd8a2487f546a3d4
Júlia Vila <juliafvila@gmail.com>
2019-04-20 18:29:37 -0300

    configurando css básico + implementando layout da lista e do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
